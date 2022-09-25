3d7b47260b21d7e7d85146c6026e174cca5e7bb1
RofiSyahrul <srofi44@gmail.com>
2022-09-25 22:36:40 +0700

    feat: add default value to theme tokens

--- src/lib/theme/styles.ts
+++ src/lib/theme/styles.ts
@@ -1,29 +1,29 @@
 export const darkStyles = `:root, ::before, ::after {
---color-bg-body: var(--color-neutral-dim);
---color-bg-subtle: var(--color-neutral-dim2);
---color-text-body: var(--color-neutral-bright);
---color-text-subtle: var(--color-neutral-bright2);
---color-text-inverse: var(--color-neutral-dim);
+--color-bg-body: var(--color-neutral-dim, black);
+--color-bg-subtle: var(--color-neutral-dim2, darkslategray);
+--color-text-body: var(--color-neutral-bright, white);
+--color-text-subtle: var(--color-neutral-bright2, lightslategray);
+--color-text-inverse: var(--color-neutral-dim, black);
 
---color-primary: var(--color-primary-bright);
---color-secondary: var(--color-secondary-bright);
---color-danger: var(--color-danger-bright);
+--color-primary: var(--color-primary-bright, cyan);
+--color-secondary: var(--color-secondary-bright, lightgreen);
+--color-danger: var(--color-danger-bright, lightcoral);
 
---color-border: var(--color-neutral-dim1);
+--color-border: var(--color-neutral-dim1, darkslategray);
 }`;
 
 export const lightStyles = `:root, ::before, ::after {
---color-bg-body: var(--color-neutral-bright);
---color-bg-subtle: var(--color-neutral-bright2);
---color-text-body: var(--color-neutral-dim);
---color-text-subtle: var(--color-neutral-dim2);
---color-text-inverse: var(--color-neutral-bright);
+--color-bg-body: var(--color-neutral-bright, white);
+--color-bg-subtle: var(--color-neutral-bright2, lightslategray);
+--color-text-body: var(--color-neutral-dim, black);
+--color-text-subtle: var(--color-neutral-dim2, darkslategray);
+--color-text-inverse: var(--color-neutral-bright, white);
 
---color-primary: var(--color-primary-dim);
---color-secondary: var(--color-secondary-dim);
---color-danger: var(--color-danger-dim);
+--color-primary: var(--color-primary-dim, darkcyan);
+--color-secondary: var(--color-secondary-dim, darkgreen);
+--color-danger: var(--color-danger-dim, darkred);
 
---color-border: var(--color-neutral-bright1);
+--color-border: var(--color-neutral-bright1, lightslategray);
 }`;
 
 export const systemStyles = `${lightStyles}
